95dad8472941d26aff9f5488d0df0a4690dc3df5
han1018 <paul52841018@hotmail.com>
2021-10-04 00:55:56 +0800

    feat : 新增try..except

--- main.py
+++ main.py
@@ -19,17 +19,29 @@ def call():
 
 def main():
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option(
+        "excludeSwitches", ['enable-automation', 'enable-logging'])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option("prefs", {
+                                    "profile.password_manager_enabled": False, "credentials_enable_service": False})
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.apple.com/tw/shop/buy-iphone/iphone-13-pro/6.1-%E5%90%8B%E9%A1%AF%E7%A4%BA%E5%99%A8-256gb-%E9%8A%80%E8%89%B2")
     driver.find_element_by_id('noTradeIn_label').click()
-    driver.find_element_by_class_name(
-        'rf-pickup-quote-storelink').is_displayed()
-    text = driver.find_element_by_xpath(
-        '/html/body/div[2]/div[5]/div[5]/div[2]/div[4]/div[2]/div[5]/div[1]/div/div[2]/div/div/div[2]/div/div/span[2]').text
-    if(text == '目前在 Apple 台北 101 缺貨' or text == ''):
-        driver.close()
-    else:
-        notify("iphone13 Pro 銀色"+text)
+    try:
+        driver.find_element_by_class_name(
+            'rf-pickup-quote-storelink').is_displayed()
+
+        text = driver.find_element_by_xpath(
+            '/html/body/div[2]/div[5]/div[5]/div[2]/div[4]/div[2]/div[5]/div[1]/div/div[2]/div/div/div[2]/div/div/span[2]').text
+
+        if(text == '目前在 Apple 台北 101 缺貨' or text == ''):
+            driver.close()
+        else:
+            notify("iphone13 Pro 銀色"+text)
+            driver.close()
+    except ValueError:
         driver.close()
 
 
